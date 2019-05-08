8d3423def6a5e95172765ea4212a25e0e729e9b4
Patrick-Ivann <33460507+Patrick-Ivann@users.noreply.github.com>
2019-05-09 00:58:15 +0200

    :hammer: UPDATE deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,8 @@ if [ $TRAVIS_BRANCH == 'master' ] ; then
   eval "$(ssh-agent -s)"
   ssh-add
   npm run build
-  rsync -rq --delete --rsync-path="mkdir -p editor-app && rsync" \
-  $TRAVIS_BUILD_DIR travis@104.248.167.41:editor-app
+  rsync -rq --delete --rsync-path="mkdir -p serverEducation && rsync" \
+  $TRAVIS_BUILD_DIR travis@104.248.167.41:serverEducation
 else
   echo "Not deploying, since this branch isn't master."
 fi
