0ad35ea108c0ee8678a874a9d55df5b81e52f545
Patrick-Ivann <33460507+Patrick-Ivann@users.noreply.github.com>
2019-07-06 15:24:04 +0200

    :hammer: JOI schema improvements --adding regex to allow space,commas,character,digit on some fields

--- src/graphql/schema/joi/article.js
+++ src/graphql/schema/joi/article.js
@@ -5,13 +5,14 @@ export default Joi.object().keys({
   
   
 
-    title:Joi.string().alphanum().max(240).required().label("titre"),
+    title:Joi.string().regex(/^\W*\w+(?:\W+\w+)*\W*$/).max(240).required().label("titre"), //accept string containing character, commas,space, digit up to 240 characters
+    subtitle:Joi.string().regex(/^\W*\w+(?:\W+\w+)*\W*$/).max(240).required().label("sous-titre"),
+    moduleId: Joi.string(),
     version:Joi.number().max(10).required().label("version"),
-    subtitle:Joi.string().max(240).required().label("sous-titre"),
-    objectives:Joi.string().max(240).required().label("objectif"),
+    objectives:Joi.string().regex(/^\W*\w+(?:\W+\w+)*\W*$/).max(400).required().label("objectif"),
     encoding:Joi.string().alphanum().max(240).required().label("encodage"),
     time:Joi.number().max(24).required().label("temps"),
-    requirements:Joi.string().max(240).required().label("pré-requis"),
-    difficulty:Joi.string().alphanum().max(50).required().label("difficulté"),
-    introduction:Joi.string().max(340).required().label("introduction"),
+    requirements:Joi.string().regex(/^\W*\w+(?:\W+\w+)*\W*$/).max(400).required().label("pré-requis"),
+    difficulty:Joi.string().regex(/^\W*\w+(?:\W+\w+)*\W*$/).max(50).required().label("difficulté"),
+    introduction:Joi.string().regex(/^\W*\w+(?:\W+\w+)*\W*$/).max(340).required().label("introduction"),
 })
--- src/graphql/schema/joi/module.js
+++ src/graphql/schema/joi/module.js
@@ -2,12 +2,13 @@ import Joi from 'joi';
 
 
 export default Joi.object().keys({
-  
-  
 
-    title:Joi.string().alphanum().max(240).required().label("titre"),
-    subtitle:Joi.string().max(240).required().label("sous-titre"),
-    introduction:Joi.string().max(340).required().label("introduction"),
-    objectives:Joi.string().max(240).required().label("objectif"),
-    requirements:Joi.string().max(240).required().label("pré-requis"),
+
+
+    title: Joi.string().regex(/^\W*\w+(?:\W+\w+)*\W*$/).max(240).required().label("titre"),
+    subtitle: Joi.string().regex(/^\W*\w+(?:\W+\w+)*\W*$/).max(240).required().label("sous-titre"),
+    courseId: Joi.string(),
+    introduction: Joi.string().regex(/^\W*\w+(?:\W+\w+)*\W*$/).max(340).required().label("introduction"),
+    objectives: Joi.string().regex(/^\W*\w+(?:\W+\w+)*\W*$/).max(400).required().label("objectif"),
+    requirements: Joi.string().regex(/^\W*\w+(?:\W+\w+)*\W*$/).max(350).required().label("pré-requis"),
 })
